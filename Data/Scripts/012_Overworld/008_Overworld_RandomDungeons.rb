d28328a8089093c7b6bfc7fa35824efe670686c5
mfoenko <mfoenko@gmail.com>
2021-09-01 23:03:45 -0400

    wip

--- Data/Scripts/012_Overworld/008_Overworld_RandomDungeons.rb
+++ Data/Scripts/012_Overworld/008_Overworld_RandomDungeons.rb
@@ -588,25 +588,26 @@ end
 Events.onMapCreate += proc { |_sender, e|
   mapID = e[0]
   map   = e[1]
-  next if !GameData::MapMetadata.exists?(mapID) ||
-          !GameData::MapMetadata.get(mapID).random_dungeon
-  # this map is a randomly generated dungeon
-  dungeon = Dungeon.new(map.width, map.height)
-  dungeon.generate
-  dungeon.generateMapInPlace(map)
-  roomtiles = []
-  # Reposition events
-  for event in map.events.values
-    tile = pbRandomRoomTile(dungeon, roomtiles)
-    if tile
-      event.x = tile[0]
-      event.y = tile[1]
-    end
-  end
-  # Override transfer X and Y
-  tile = pbRandomRoomTile(dungeon, roomtiles)
-  if tile
-    $game_temp.player_new_x = tile[0]
-    $game_temp.player_new_y = tile[1]
-  end
+  echoln "map #{mapID} #{map}"
+  # next if !GameData::MapMetadata.exists?(mapID) ||
+  #         !GameData::MapMetadata.get(mapID).random_dungeon
+  # # this map is a randomly generated dungeon
+  # dungeon = Dungeon.new(map.width, map.height)
+  # dungeon.generate
+  # dungeon.generateMapInPlace(map)
+  # roomtiles = []
+  # # Reposition events
+  # for event in map.events.values
+  #   tile = pbRandomRoomTile(dungeon, roomtiles)
+  #   if tile
+  #     event.x = tile[0]
+  #     event.y = tile[1]
+  #   end
+  # end
+  # # Override transfer X and Y
+  # tile = pbRandomRoomTile(dungeon, roomtiles)
+  # if tile
+  #   $game_temp.player_new_x = tile[0]
+  #   $game_temp.player_new_y = tile[1]
+  # end
 }
